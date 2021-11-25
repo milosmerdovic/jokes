91c07d6a93c0fb4113455ae6e02174928a7d8a79
Garfi88 <d.madjarev@gmail.com>
2021-11-25 15:30:32 +0100

    Initial changes in JokesRepository.java, JokesController.java and application.properties files

--- src/main/java/com/zezalice/joke/controllers/JokesController.java
+++ src/main/java/com/zezalice/joke/controllers/JokesController.java
@@ -1,5 +1,22 @@
 package com.zezalice.joke.controllers;
 
+import com.zezalice.joke.entities.Jokes;
+import com.zezalice.joke.repositories.JokesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class JokesController {
-    
+
+    @Autowired
+    private JokesRepository jokesRepository;
+
+    @GetMapping("/jokes")
+    public String listAll(Model model) {
+        List<Jokes> listJokes = (List<Jokes>) jokesRepository.findAll();
+        model.addAttribute("listJokes", listJokes);
+        return "jokes";
+    }
 }
--- src/main/java/com/zezalice/joke/entities/Categories.java
+++ src/main/java/com/zezalice/joke/entities/Categories.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Categories {
-    
+
     @Id
     @GeneratedValue
     private int id;
